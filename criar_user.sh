81dc9af7f2e7ebdeab97eed621366c311545b25c
Ricardo Barreto Souza Santos <ricbarreto27@gmail.com>
2025-03-18 15:14:18 -0300

    Update criar_user.sh

--- criar_user.sh
+++ criar_user.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Verifica se o script está sendo executado como root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Este script deve ser executado como root."
+    exit 1
+fi
 
 echo "Criando usuários do sistema...."
 
+# Criando os usuários do sistema com senha criptografada
 useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
-passwd guest10 -e
+passwd guest10 -e  # A senha expira imediatamente, forçando o usuário a trocar a senha no primeiro login
 
 useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
 passwd guest11 -e
